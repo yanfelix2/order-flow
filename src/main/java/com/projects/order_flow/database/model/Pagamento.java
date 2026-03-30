4fdc5bcab16c47f00963cd423ded6485d6176a9b
Yan Felix <yanfelixxluis@gmail.com>
2026-03-29 23:50:07 -0300

    refactor(model): altera estratégia de geração de ID para IDENTITY em Pagamento

--- src/main/java/com/projects/order_flow/database/model/Pagamento.java
+++ src/main/java/com/projects/order_flow/database/model/Pagamento.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 public class Pagamento {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @OneToOne
